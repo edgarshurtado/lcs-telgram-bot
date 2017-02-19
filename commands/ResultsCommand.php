d62ed8887bb57e5c63145d837248800e70130b09
Edgar S. Hurtado <edsanhu@gmail.com>
2017-02-19 20:00:43 +0100

    Implement basic message with games results

--- commands/ResultsCommand.php
+++ commands/ResultsCommand.php
@@ -16,8 +16,70 @@ class ResultsCommand extends UserCommand
 
         $data = [];                               // Set up the new message data
         $data['chat_id'] = $chat_id;              // Set Chat ID to send the message to
-        $data['text'] = 'This is just a Test...'; // Set message to send
+
+        $games_results = $this->getResults();
+
+        $data['text'] = $this->formatMessage($games_results); // Set message to send
 
         return Request::sendMessage($data);       // Send message!
+    }
+
+    private function getResults()
+    {
+
+        $conn = new \PDO("mysql:dbname=lcs_results_bot;host=127.0.0.1", "root", "");
+
+        $sql = "
+            SELECT blueTeam.name blue_team, redTeam.name red_team,
+            winner.name winner, lr.week week
+            FROM lcs_results lr
+            JOIN lcs_teams blueTeam on lr.team_blue = blueTeam.id
+            JOIN lcs_teams redTeam on lr.team_red = redTeam.id
+            JOIN lcs_teams winner on lr.winner = winner.id
+
+        ";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+
+        return $results;
+    }
+
+    private function formatMessage($lcs_results_array)
+    {
+        $formatted_array = [];
+
+        $message = "";
+
+        foreach ($lcs_results_array as $match) {
+            $formatted_array[$match["week"]][] = [
+                "team_blue" => $match["blue_team"],
+                "team_red" => $match["red_team"],
+                "winner" => $match["winner"]
+            ];
+        }
+
+        foreach ($formatted_array as $week => $matches) {
+            $message .= "WEEK $week\n------------\n";
+            foreach ($matches as $match) {
+                $team_blue = ucwords($match["team_blue"], " ");
+                $team_red = ucwords($match["team_red"], " ");
+                $winner = ucwords($match["winner"]);
+
+                if($team_blue == $winner){
+                    $team_blue .= " (W)";
+                }else{
+                    $team_red .= " (W)";
+                }
+
+                $message .= $team_blue . " Vs. " . $team_red . "\n";
+            }
+            $message .= "\n";
+        }
+
+        return $message;
     }
 }
--- getUpdatesCLI.php
+++ getUpdatesCLI.php
@@ -4,19 +4,22 @@ require __DIR__ ."/config.php";
 
 $commands_folder = __DIR__ ."/commands/";
 
-try {
-    // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+while (true) {
+  try {
+      // Create Telegram API object
+      $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
 
-    // Register commands folder
-    $telegram->addCommandsPath($commands_folder);
+      // Register commands folder
+      $telegram->addCommandsPath($commands_folder);
 
-    // Enable MySQL
-    $telegram->enableMySQL($mysql_credentials);
+      // Enable MySQL
+      $telegram->enableMySQL($mysql_credentials);
 
-    // Handle telegram getUpdate request
-    $telegram->handleGetUpdates();
-} catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // log telegram errors
-    echo $e;
+      // Handle telegram getUpdate request
+      $telegram->handleGetUpdates();
+  } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+      // log telegram errors
+      echo $e;
+  }
+  sleep(1);
 }
