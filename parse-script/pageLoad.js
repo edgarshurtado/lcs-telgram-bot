fa27bb56030ceaf2a6f17a8365240eb4961e1b3e
Edgar S. Hurtado <edsanhu@gmail.com>
2017-02-28 20:35:43 +0100

    Implement logic for url construction
    Now it will allow the url construcction when a week number is passed to
    the script.

--- parse-script/pageLoad.js
+++ parse-script/pageLoad.js
@@ -3,10 +3,15 @@ var system = require('system');
 
 checkArguments(system.args);
 
+var week = system.args.length === 2
+            ? system.args[1]
+            : -1;
 
 var url = "http://www.lolesports.com/en_US/eu-lcs/eu_2017_spring/schedule/regular_season";
 
-url += "/" + week;
+if(week >= 1 ){
+    url += "/" + week;
+}
 
 page.open( url, function () {
     setTimeout(function(){
