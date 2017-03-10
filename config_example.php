e2d5c1e8302e510f06f9f5d035c044fe035ada1e
Edgar S. Hurtado <edsanhu@gmail.com>
2017-03-10 22:30:14 +0100

    Add config_example file

---
+++ config_example.php
@@ -0,0 +1,13 @@
+<?php
+
+$mysql_credentials =
+[
+   'host'     => '127.0.0.1',
+   'user'     => 'root',
+   'password' => NULL,
+   'database' => 'your_database',
+];
+
+$BOT_NAME = 'bot_name';
+
+$API_KEY = 'telegram_api';
